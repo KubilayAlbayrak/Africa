d5e884e5d337a2108b0ff59658e660141d481fa1
Kubilay <skubilayalbayrak@gmail.com>
2021-09-13 21:26:26 +0300

    Initial Commit

---
+++ Shared/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Shared
+//
+//  Created by Kubilay Albayrak on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
